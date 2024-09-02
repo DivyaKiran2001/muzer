1f5f84f3740526fbfbd90ff0b0d3ae0cf61e7b4a
DivyaKiran2001 <divyakirantatikonda@gmail.com>
2024-09-02 20:18:51 +0530

    part 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth/next";
 import { NextResponse } from "next/server";
 import GoogleProvider from "next-auth/providers/google";
+import { prismaClient } from "@/app/lib/db";
+// import { Provider } from "@prisma/client";
 
 
 const handler = NextAuth({
@@ -9,7 +11,29 @@ const handler = NextAuth({
           clientId: process.env.GOOGLE_CLIENT_ID ?? "",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-      ]
+      ],
+      callbacks : {
+        async signIn(params) {
+          if(!params.user.email) {
+            return false;
+          }
+
+          console.log(params);
+          try {
+            await prismaClient.user.create({
+              data : {
+                email : params.user.email,
+                provider: "Google"
+              }
+            })
+          }
+          catch(e)
+          {
+
+          }
+          return true;
+        }
+      }
 })
 
 export {handler as GET,handler as POST}
